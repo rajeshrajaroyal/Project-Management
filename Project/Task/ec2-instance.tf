92b5a561ffe4256e965251e9c89f5d670030a5fa
rajeshrajaroyal <78246917+rajeshrajaroyal@users.noreply.github.com>
2024-11-30 11:52:40 +0530

    Update ec2-instance.tf

--- Project/Task/ec2-instance.tf
+++ Project/Task/ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instance" {
   ami = "ami-022ce6f32988af5fa"
   instance_type = "t2.micro"
   key_name = "harishsoftware"
-  vpc_security_group_ids = ["sg-0bf7610b676ac434a"]
+  vpc_security_group_ids = ["sg-081dab0d0fbecad59"]
   tags = {
     Name = "Webserver"
   }
